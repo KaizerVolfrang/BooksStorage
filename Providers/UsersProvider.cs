115c76f7ef809eb3cb7e294774cbb5be063bcf05
Vlad <vlad@gmail.com>
2020-12-15 20:14:09 +0400

    repos1

---
+++ Providers/UsersProvider.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using BooksProject.Models;
+
+namespace BooksProject.Providers
+{
+    public class UsersProvider
+    {
+        private SqlConnection _connection;
+
+        public UsersProvider(SqlConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public User GetAll()
+        {
+            var result = new User();
+
+            try
+            {
+                _connection.Open();
+                var command = new SqlCommand(@"
+                SELECT * FROM
+                    [Users]
+                WHERE
+                    [Login] = @Login
+                ",
+                    _connection);
+                using(var reader = command.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        User user = new User()
+                        {
+                            Id = reader.GetInt32(0),
+                            Login = reader.GetString(1),
+                            Password = reader.GetString(2),
+                            Role = reader.GetString(3)
+                        };
+                        result = user;
+                    }
+                }
+            }
+            finally
+            {
+                _connection.Close();
+            }
+            return result;
+        }
+    }
+}
---
+++ Views/SignInForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace BooksProject.Views
+{
+    partial class SignInForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.signInButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Логин";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Пароль";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.Location = new System.Drawing.Point(41, 60);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(197, 20);
+            this.loginTextBox.TabIndex = 2;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(41, 153);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(197, 20);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // signInButton
+            // 
+            this.signInButton.Location = new System.Drawing.Point(41, 216);
+            this.signInButton.Name = "signInButton";
+            this.signInButton.Size = new System.Drawing.Size(109, 46);
+            this.signInButton.TabIndex = 4;
+            this.signInButton.Text = "Войти";
+            this.signInButton.UseVisualStyleBackColor = true;
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(256, 216);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(104, 46);
+            this.closeButton.TabIndex = 5;
+            this.closeButton.Text = "Выйти";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
+            // 
+            // SignInForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 274);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.signInButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "SignInForm";
+            this.Text = "SignInForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button signInButton;
+        private System.Windows.Forms.Button closeButton;
+    }
+}
